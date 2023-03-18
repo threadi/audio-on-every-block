7e146230ba087f3851117f68eea2bab5973fe289
Thomas Zwirner <tzwirne@gmx.de>
2023-03-18 13:58:10 +0100

    change title output

--- templates/audio-on-every-block.php
+++ templates/audio-on-every-block.php
@@ -1,3 +1,3 @@
 <figure class="audio-on-every-block-output">
-    <audio src="<?php echo esc_attr($args['file']); ?>" controls<?php echo esc_attr($args['title']); ?>></audio>
+    <audio src="<?php echo esc_attr($args['file']); ?>" controls title="<?php echo esc_attr($args['title']); ?>"></audio>
 </figure>
